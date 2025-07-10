7da3724e6e10a7cb6725cd97a826cc8a29fe7b7e
Brenda Carolina Del Toro Santana <brenda.carolina.dts@gmail.com>
2025-07-10 17:00:59 +0000

    3nd Cmm

--- feed.py
+++ feed.py
@@ -20,7 +20,7 @@ xml_tree.SubElement(channel_element, 'description').text = yaml_data['descriptio
 xml_tree.SubElement(channel_element, 'itunes:image',{'href': link_prefix + yaml_data['image']})
 xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
 xml_tree.SubElement(channel_element, 'link').text = link_prefix
-xml_tree.SubElement(channel_element, 'itynes:category',{'text':yaml_data['category']})
+xml_tree.SubElement(channel_element, 'itunes:category',{'text':yaml_data['category']})
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
 
 
